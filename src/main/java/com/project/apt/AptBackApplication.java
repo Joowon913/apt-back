c9d605473f6240918e897b1def6448f82e901d14
hola <143702392+Joowon913@users.noreply.github.com>
2024-03-11 11:38:04 +0900

    apt-back 생성

---
+++ src/main/java/com/project/apt/AptBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.apt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apt/AptBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
